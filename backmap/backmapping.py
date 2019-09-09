7d4d9788b3887222ae1604822207255d7cf03ff6
Kirill Shmilovich <kshmilov@gmail.com>
2019-09-09 13:34:53 -0700

    Added random placement backmapping

--- backmap/backmapping.py
+++ backmap/backmapping.py
@@ -6,31 +6,54 @@ Handles the primary functions
 ##
 TODO
 ----
--> Find some way to convert gro and itp files to pdb files easily for user
--> Checker that determins chemical fesability based on graph isomorphims
--> Think about how to feed in AA information
+-> Generalize to arbitrary number of different molecules
 """
 import mdtraj as md
 from .utils import *
 
+
 class Backmap():
 
-    def __init__(self, CG_pdb_f_name, AA_pdb_f_name):
-        self.CG_trj = md.load_pdb(filename=CG_pdb_f_name).remove_solvent()
-        self.CG_top = self.CG_trj.top
-        self.CG_molecules = np.array([list(mol) for mol in self.CG_top.find_molecules()])
-        self.AA_trj = md.load_pdb(filename=AA_pdb_f_name).remove_solvent()
-        self.AA_top = self.AA_trj.top
-    
-    @property
-    def CG_residues(self):
-        residues = np.array(list(self.CG_top.residues))
-        return residues
-    
-    @property
-    def CG_res_names(self):
-        residues = self.CG_residues
-        return np.unique(np.array([res.name for res in residues]))
+    def __init__(self, CG_pdb_f_name, AA_pdb_f_name): 
+        self.CG_pdb_f_name = CG_pdb_f_name
+        self.AA_pdb_f_name = AA_pdb_f_name
 
-    def residue_mappings(self):
-        pass
+        self.CG_trj = md.load_pdb(filename=self.CG_pdb_f_name).remove_solvent()
+        self.CG_top = self.CG_trj.top
+        self.AA_trj = md.load_pdb(filename=self.AA_pdb_f_name).remove_solvent()
+        self.AA_top = self.AA_trj.top
+
+        self.AA_new_trj = md.Trajectory(self.AA_trj.xyz, self.AA_trj.topology) 
+        self.CG_beads = self._parse_CG_pdb(self.CG_pdb_f_name)
+        self.AA_beads = self._parse_AA_pdb(self.AA_pdb_f_name)
+    
+    def backmap(self, output_f_name = None):
+        if output_f_name is None:
+            self.output_f_name = self.AA_pdb_f_name.split(".")[0] + "_backmapped.pdb"
+        else:
+            self.output_f_name = output_f_name
+
+        for bead in self.CG_beads:
+            inds = index_AA_name(self.AA_beads, bead)
+            target_com = atom_name_COM(self.CG_trj, bead)
+            shift_COM(self.AA_new_trj, inds, target_com)
+        
+        self.AA_new_trj.save_pdb(self.output_f_name)
+
+    def _parse_CG_pdb(self, CG_pdb_f_name):
+        CG_bead = list()
+        with open(CG_pdb_f_name) as f:
+            for line in f:
+                split_line = line.split()
+                if (split_line[0] == "HETATM") or (split_line[0] == "ATOM"):
+                    CG_bead.append(split_line[2])
+        return np.array(CG_bead)
+
+    def _parse_AA_pdb(self, AA_pdb_f_name):
+        AA_bead = list()
+        with open(AA_pdb_f_name) as f:
+            for line in f:
+                split_line = line.split()
+                if (split_line[0] == "HETATM") or (split_line[0] == "ATOM"):
+                    AA_bead.append(split_line[-1])
+        return np.array(AA_bead)
--- backmap/utils.py
+++ backmap/utils.py
@@ -1,31 +1,18 @@
 import numpy as np
-from networkx.algorithms import isomorphism
+import mdtraj as md
 
-def get_res_xyz(trj, res):
-    return trj.atom_slice([atom.index for atom in res.atoms]).xyz
+def COM(trj, inds):
+    return md.compute_center_of_mass(trj.atom_slice(inds))
 
-def element_pairs_check(d):
-    keys = np.array([atom.element.symbol for atom in d.keys()])
-    vals = np.array([atom.element.symbol for atom in d.values()])
-    return (keys == vals).all()
+def index_atom_name(trj, name):
+    return np.where(name == np.array([atom.name for atom in trj.top.atoms]))[0]
 
-def get_isomorphic_mappings(main, sub):
-    main_bg = main.to_bondgraph()
-    sub_bg = sub.to_bondgraph()
-    GM = isomorphism.GraphMatcher(main_bg, sub_bg)
-    if GM.subgraph_is_isomorphic():
-        pairs = np.array(list(GM.subgraph_isomorphisms_iter()))
-        pair_bool = np.array([element_pairs_check(d) for d in pairs])
-        pairs = pairs[pair_bool] 
-    else:
-        return None
-    atom_inds = np.array([np.sort([atom.index for atom in m.keys()]) for m in pairs])
-    _,unique_inds = np.unique(atom_inds, axis=0, return_index=True)
-    return pairs[unique_inds]
+def atom_name_COM(trj, name):
+    inds = index_atom_name(trj, name)
+    return COM(trj, inds)
 
-def remove_atoms_from_top(main, atoms):
-    """ Main is a topology object"""
-    atom_inds = np.arange(main.n_atoms)
-    remove_inds = np.array([atom.index for atom in atoms]) 
-    new_top = main.subset(np.delete(atom_inds, remove_inds))
-    return new_top
+def shift_COM(trj, inds, target_com):
+    trj.xyz[:, inds] += target_com - COM(trj, inds)
+
+def index_AA_name(AA_bead, name):
+    return np.where(name == AA_bead)[0]
