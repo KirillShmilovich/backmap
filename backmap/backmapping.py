764291416de8da24a79988d448bcc47a99fb648c
Kirill Shmilovich <kshmilov@gmail.com>
2019-12-25 16:36:04 -0600

    ff

--- backmap/backmapping.py
+++ backmap/backmapping.py
@@ -20,7 +20,7 @@ class Backmapping:
     def __init__(self, CG_pdb_fname, FG_pdb_fname):
         self.map = Map(FG_pdb_fname, CG_pdb_fname)
 
-    def backmap(self, to_back_fname, output_fname=None, mode="COM"):
+    def backmap(self, to_back_fname, output_fname=None, mode="COM", **kwargs):
         if output_fname is None:
             self.output_fname = to_back_fname.split(".")[0] + "_backmapped.pdb"
         else:
@@ -31,9 +31,12 @@ class Backmapping:
 
         if mode == "COM":
             self.Backmap = COM_backmap(self.CG_to_back, self.map)
+            self.Backmap.backmap(**kwargs)
             self.FG_back_trj = self.Backmap.FG_back_trj
         else:
-            raise NotImplementedError("Only 'COM' backmapping is supported")
+            raise NotImplementedError(
+                "Only center of mass (COM) backmapping is supported"
+            )
 
         print(f"Writing output to {self.output_f_name}")
         self.FG_back.save_pdb(self.output_f_name)
